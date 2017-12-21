0590a00fdd854b0e4fa1348969a89344b32f3534
Daniel Graf <grafmark59@gmail.com>
2017-12-21 17:59:05 -0500

    added scrolling to sections

--- js/init.js
+++ js/init.js
@@ -1,8 +1,30 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav(options={closeOnClick: true});
     $('.parallax').parallax();
+    $('.scrollspy').scrollSpy(options={scrollOffset:0});
+    //
+    $("#menu li a[href^='#']").on('click', function(e) {
+
+         // prevent default anchor click behavior
+         e.preventDefault();
+
+         // store hash
+         var hash = this.hash;
+
+         // animate
+         $('html, body').animate({
+             scrollTop: $(hash).offset().top-50
+           }, 500, function(){
+
+             // when done, add hash to url
+             // (default click behaviour)
+             // window.location.hash = hash;
+           });
+
+      });
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
